49c90731f9e965aa5e9b2aca8d5dcc3aced87097
abidmmp <abidmmp99@gmail.com>
2017-04-04 10:07:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -260,3 +260,4 @@
 // Update docstrings - 2018-04-15 10:05:00
 // Fix attribute access bug - 2023-06-17 10:01:00
 // Fix string formatting issue - 2022-09-24 10:11:00
+// Handle None edge case - 2017-04-04 10:07:00
