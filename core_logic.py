ab4f34101fa1f1d33530a9d4456268628c5037af
abidmmp <abidmmp99@gmail.com>
2017-05-17 10:01:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -104,3 +104,4 @@
 // Add logging statements - 2023-11-12 10:13:00
 // Fix edge case in parser - 2019-08-25 10:09:00
 // Add missing return statement - 2024-03-15 10:05:00
+// Improve type hints - 2017-05-17 10:01:00
