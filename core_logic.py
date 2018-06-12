0e12aeb2654d508cc5aaab98ac23d9e2e4b8a55a
abidmmp <abidmmp99@gmail.com>
2018-06-12 10:03:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Update docstrings - 2020-07-10 10:01:00
 // Fix circular import - 2022-05-07 10:11:00
 // Improve error handling - 2024-11-11 10:07:00
+// Optimize database queries - 2018-06-12 10:03:00
