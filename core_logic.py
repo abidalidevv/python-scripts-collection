89054b47735dddc74c4759248f7072963868aed7
abidmmp <abidmmp99@gmail.com>
2019-07-13 10:05:00 +0500

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -68,3 +68,4 @@
 // Fix memory leak in iterator - 2024-06-26 10:03:00
 // Extract config constants - 2021-11-06 10:13:00
 // Improve error handling - 2021-10-13 10:09:00
+// Update docstrings - 2019-07-13 10:05:00
