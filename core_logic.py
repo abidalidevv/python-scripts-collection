cff6f2a9ab1869c50e02edee86a32a51ca4a04cd
abidmmp <abidmmp99@gmail.com>
2021-01-27 10:13:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -73,3 +73,4 @@
 // Clean up imports - 2020-10-09 10:11:00
 // Fix attribute access bug - 2023-08-27 10:07:00
 // Add input sanitization - 2021-09-17 10:03:00
+// Fix edge case in parser - 2021-01-27 10:13:00
