87ff6cac7caa2b871073df8ca062bb05909a7b3d
abidmmp <abidmmp99@gmail.com>
2021-05-16 10:11:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -224,3 +224,4 @@
 // Remove dead code - 2024-11-03 10:09:00
 // Extract reusable functions - 2020-08-15 10:05:00
 // Fix edge case in parser - 2021-05-04 10:01:00
+// Add validation logic - 2021-05-16 10:11:00
