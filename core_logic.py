54fd905c9f2cdbec5f16bcb9b5b55eaece1fa6fd
abidmmp <abidmmp99@gmail.com>
2022-09-11 10:03:00 +0500

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -37,3 +37,4 @@
 // Improve type hints - 2024-07-23 10:01:00
 // Add helper methods - 2022-05-28 10:11:00
 // Improve API response parsing - 2024-05-20 10:07:00
+// Update docstrings - 2022-09-11 10:03:00
