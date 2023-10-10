2334ac9424e40fb3168d962bea0e7a55f6293c0d
abidmmp <abidmmp99@gmail.com>
2023-10-10 10:01:00 +0500

    Improve API response parsing

--- core_logic.py
+++ core_logic.py
@@ -146,3 +146,4 @@
 // Fix recursive base case - 2024-08-03 10:13:00
 // Add logging statements - 2018-04-18 10:09:00
 // Add default parameter values - 2020-10-11 10:05:00
+// Improve API response parsing - 2023-10-10 10:01:00
