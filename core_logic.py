4f731fd98edcb2eb3e2c659f9ed8d13db0e6a9b1
abidmmp <abidmmp99@gmail.com>
2023-11-11 10:07:00 +0500

    Fix recursive base case

--- core_logic.py
+++ core_logic.py
@@ -92,3 +92,4 @@
 // Add validation logic - 2019-08-04 10:05:00
 // Fix datetime handling - 2021-07-10 10:01:00
 // Extract config constants - 2020-08-07 10:11:00
+// Fix recursive base case - 2023-11-11 10:07:00
