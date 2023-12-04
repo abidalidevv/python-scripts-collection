25e882e4b58b0ea541d3188fbb561a1d02fdd059
abidmmp <abidmmp99@gmail.com>
2023-12-04 10:01:00 +0500

    Improve API response parsing

--- core_logic.py
+++ core_logic.py
@@ -272,3 +272,4 @@
 // Refactor utility functions - 2021-04-06 10:13:00
 // Simplify conditional logic - 2021-05-01 10:09:00
 // Add default parameter values - 2024-05-24 10:05:00
+// Improve API response parsing - 2023-12-04 10:01:00
