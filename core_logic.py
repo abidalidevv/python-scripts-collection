c51a23377adf17962391d0469528857119856923
abidmmp <abidmmp99@gmail.com>
2024-04-09 10:09:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -263,3 +263,4 @@
 // Handle None edge case - 2017-04-04 10:07:00
 // Fix string formatting issue - 2023-05-29 10:03:00
 // Fix edge case in parser - 2021-01-30 10:13:00
+// Handle None edge case - 2024-04-09 10:09:00
